2193059f7e1f18d0670603c41e4c0a8f0693a849
ZXHHHH123 <2242347823@qq.com>
2017-08-23 11:06:13 +0800

    top-bar

--- src/components/top-bar.vue
+++ src/components/top-bar.vue
@@ -1,10 +1,43 @@
 <template>
   <div class="top-bar">
-    top-bar
-  </div>
+      <el-menu theme="dark" :default-active="activeIndex" class="header-slider" mode="horizontal" @select="handleSelect" style="position: fixed;;width: 100%;">
+        <el-menu-item index="1" class="title">金融信息管理平台</el-menu-item>
+        <el-menu-item  class="admin" index="3"><el-tooltip class="item" content="注销" placement="bottom" effect="light"><el-button type="text">Admin</el-button>  </el-tooltip>
+        </el-menu-item>
+        <div>123</div>
+      </el-menu>
+    </div>
 </template>
 <script lang="babel">
-export default {
+  import ElCol from 'element-ui/packages/col/src/col'
+  import ElRow from 'element-ui/packages/row/src/row'
 
+  export default {
+    components: {
+      ElRow,
+      ElCol
+    },
+    data () {
+      return {
+        activeIndex: '1',
+        activeIndex2: '1'
+      }
+    },
+
+    methods: {
+      handleSelect (key, keyPath) {
+        console.log(key, keyPath)
+      }
+    }
 }
 </script>
+
+<style>
+  body{
+    margin-left: 0px;
+    margin-top: 0px
+  }
+  .top-bar{position: fixed;top: 0;}
+  .title{border-bottom:none !important;font-size:2rem;font-family:kaiti;}
+  .admin{border-bottom:none !important;float: right !important;}
+</style>
